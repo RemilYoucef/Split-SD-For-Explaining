437b6fc6fc21e5cf957244cece36e9df23fcf3b3
youcef <fy_remil@esi.dz>
2020-08-05 10:46:22 +0100

    add setups

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+  name = 'splitsd',
+  packages = ['splitsd'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Split SD For Explaining',
+  author = 'Youcef REMIL',
+  author_email = 'fy_remil@esi.dz',
+  url = 'https://github.com/RemilYoucef/Split-SD-For-Explaining'
+)
